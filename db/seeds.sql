ffc59df768b746aceba9944bac73a2a279e1daee
rlau4 <p.lau.004@gmail.com>
2019-06-26 21:17:08 -0400

    updated db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name) VALUES ("Bacon Double Cheese Burger");
-INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
-INSERT INTO burgers (burger_name) VALUES ("Kiddy Cheese Burger");
-INSERT INTO burgers (burger_name) VALUES ("Black and Blue Burger");
+INSERT INTO xa60mag1gea6gux3.burgers (burger_name) VALUES ("Bacon Double Cheese Burger");
+INSERT INTO xa60mag1gea6gux3.burgers (burger_name) VALUES ("Veggie Burger");
+INSERT INTO xa60mag1gea6gux3.burgers (burger_name) VALUES ("Kiddy Cheese Burger");
+INSERT INTO xa60mag1gea6gux3.burgers (burger_name) VALUES ("Black and Blue Burger");
